b052a83efb81b6a7ac1ecf45ec6db9f890f63ec5
eldy020502 <96163277+eldy020502@users.noreply.github.com>
2022-02-05 23:47:32 +0800

    Add files via upload

---
+++ SiestaRobot/modules/alive.py
@@ -0,0 +1,24 @@
+import os
+import re
+from platform import python_version as kontol
+from telethon import events, Button
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from SiestaRobot.events import register
+from SiestaRobot import telethn as tbot
+
+
+PHOTO = "https://telegra.ph/file/32ca8914d2a42d50fc52a.jpg"
+
+@register(pattern=("/alive"))
+async def awake(event):
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Venz Robot.** \n\n"
+  TEXT += "💠 **I'm Working Properly** \n\n"
+  TEXT += f"💠 **My Master : [VnotV](https://t.me/vnotv)** \n\n"
+  TEXT += f"💠 **Library Version :** `{telever}` \n\n"
+  TEXT += f"💠 **Telethon Version :** `{tlhver}` \n\n"
+  TEXT += f"💠 **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT += "**Thanks For Adding Me Here ❤️**"
+  BUTTON = [[Button.url("Help", "https://t.me/venzastreambot?start=help"), Button.url("Support", "https://t.me/justvenzzz")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
